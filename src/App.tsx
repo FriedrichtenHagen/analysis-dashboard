0f395bc26188af60210c567df73dec08a18d120d
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-07-27 21:12:04 +0200

    Add chart.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import './App.css'
-import { Component } from './components/table'
+import { Table } from './components/Table'
+import { Chart } from './components/Chart'
+import { useState } from 'react'
+import createMockData  from './createMockData'
+
 function App() {
+  
+  const startDate = new Date(2023, 1, 1)
+  const mockData = createMockData(50, startDate)
+  const [data] = useState(mockData)
 
 
   return (
     <>
-      
-
-
-
-      <Component/>
-
-
-      
+      <Chart data1={data}/>
+      <Table data1={data}/>
     </>
   )
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,48 +3,12 @@ import { useTheme } from '@table-library/react-table-library/theme';
 import { getTheme } from '@table-library/react-table-library/baseline';
 import { useSort } from "@table-library/react-table-library/sort";
 
-import createMockData  from '../createMockData'
-
-
-// const exampleList = [
-//     {
-//       id: '1',
-//       meta: 1234.54,
-//       date: new Date(2020, 1, 17),
-//       type: 'SETUP',
-//       isComplete: true,
-//     },
-//     {
-//       id: '2',
-//       meta: 6234.43,
-//       date: new Date(2020, 2, 28),
-//       type: 134132,
-//       isComplete: true,
-//     },
-//     {
-//       id: '3',
-//       meta: 1556.76,
-//       date: new Date(2020, 3, 8),
-//       type: 'LEARN',
-//       isComplete: false,
-//     },
-//     {
-//         id: '3',
-//         meta: 7654.54,
-//         date: new Date(2020, 3, 8),
-//         type: 'LEARN',
-//         isComplete: false,
-//     }
-// ];
-
-const startDate = new Date(2023, 1, 1)
-const mockDate = createMockData(50, startDate)
 
 
 
-export const Component = () => {
+export const Table = ({data1}) => {
   const theme = useTheme(getTheme());
-  const data = { nodes:mockDate };
+  const data = { nodes:data1 };
 
   const sort = useSort(
     data,
