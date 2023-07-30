7cb4e880c60587af92d40bbbc756417d9bb65dc7
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-07-30 21:45:04 +0200

    Add date range picker from rsuitejs.

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,17 +1,10 @@
+import { DateRangePicker } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
 
-import { useState } from 'react';
-import DateRangePicker from '@wojtekmaj/react-daterange-picker';
-
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export function DatePicker() {
-  const [value, onChange] = useState<Value>([new Date(), new Date()]);
 
   return (
-    <div>
-      <DateRangePicker onChange={onChange} value={value} />
-    </div>
+    <DateRangePicker />
   );
 }
