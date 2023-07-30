1e159e69b740b431cbcad0111b4366c3cb4aa1c2
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-07-30 22:18:55 +0200

    Start implementing Context for date range.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { Chart } from './components/Chart'
 import { DatePicker } from './components/DatePicker'
 import { useState } from 'react'
 import createMockData  from './createMockData'
+import { createContext } from 'react';
 
 
 
 function App() {
-  
+  const [dataRange, setDataRange] = useState([]);
+  const DateRangeContext = createContext(dataRange);
+
+
+
   const startDate = new Date(2023, 1, 1)
   const mockData = createMockData(30, startDate)
   const [data] = useState(mockData)
@@ -15,9 +20,11 @@ function App() {
 
   return (
     <>
-        <DatePicker/>
-        <Chart data1={data}/>
-        <Table data1={data}/>
+        <DateRangeContext.Provider value={dataRange}>
+          <DatePicker setDataRange={setDataRange}/>
+          <Chart data1={data}/>
+          <Table data1={data}/>
+        </DateRangeContext.Provider>
     </>
   )
 }
--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,10 +1,18 @@
 import { DateRangePicker } from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
+import { useState, useContext } from "react";
+
+export function DatePicker({ setDataRange }) {
+  const dataRangeContext = useContext(dataRangeContext);
+  
+  const [value, setValue] = useState([
+    new Date('2017-02-01 01:00:00'),
+    new Date('2017-02-02 14:00:00')
+  ]);
 
 
-export function DatePicker() {
 
   return (
-    <DateRangePicker />
+    <DateRangePicker value={value} onChange={setDataRange} />
   );
 }
