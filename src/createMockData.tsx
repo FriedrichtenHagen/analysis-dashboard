c6670ca55085f04f3dd0e331fca2f37297c55cb0
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-08-05 21:48:33 +0200

    Write function that sums up the days to create monthly data.

--- src/createMockData.tsx
+++ src/createMockData.tsx
@@ -25,6 +25,49 @@ export default function createMockData(dataSetNumber:number, startDate:Date){
     //console.log(mockJson)
     return mockJson
 }
+const dataSetNumber = 1;
+const startDate = new Date;
+let mockJson = createMockData(dataSetNumber, startDate)
+
+convertDataToMonthly(mockJson)
+
+
+function convertDataToMonthly(mockJson:any){
+    let monthlyData:any = [];
+
+    for(let i=0; i<mockJson.length; i++){
+        let addedDay = mockJson[i]
+        let month = addedDay.date.getMonth()+1 // months are index starting at 0
+        let year = addedDay.date.getFullYear()
+        if(!monthlyData.some(e => e.month === month && e.year === year)){
+            // the object for this month does not exist yet
+            monthlyData.push(
+                {
+                    id: monthlyData.length,
+                    month: month,
+                    year: year, 
+                    meta_spend: addedDay.meta_spend,
+                    google_spend: addedDay.google_spend,
+                    revenue: addedDay.revenue,
+                    influencer: addedDay.influencer,
+                }
+            )
+        } else{
+            // the month object exists and values are added to it
+            // find the correct month for adding values
+            let correctMonthObject = monthlyData.find((monthObj:any) => monthObj.month === month)
+            // add day data to month object
+            correctMonthObject.meta_spend += addedDay.meta_spend
+            correctMonthObject.google_spend += addedDay.google_spend
+            correctMonthObject.revenue += addedDay.revenue
+            correctMonthObject.influencer += addedDay.influencer
+        }
+    }
+    console.log(monthlyData)
+    return monthlyData
+}
+
+
 
 export interface dataDay {
     date: Date,
@@ -36,4 +79,13 @@ export interface dataDay {
 }
 export interface dataDayProps {
     data1: dataDay[]
+}
+interface dataMonth {
+    month: string, 
+    year: string, 
+    google_spend: number, 
+    id: number, 
+    meta_spend: number, 
+    revenue: number,
+    influencer: number,
 }
