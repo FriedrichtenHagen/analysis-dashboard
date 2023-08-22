a17d707cd40c3389daf1bcb6c3767fd361ea8566
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-08-22 21:53:28 +0200

    Set up function to calculate difference between periods.

--- src/components/GridNumberBox.tsx
+++ src/components/GridNumberBox.tsx
@@ -9,43 +9,74 @@ export function GridNumberBox({ data }:any){
         influencer_sum: 0,            
         revenue_sum: 0,
     })
-    const head1 = 'Google Spend'
-    const head2 = 'Facebook Spend'
-    const head3 = 'Influencer'
-    const head4 = 'Revenue'
-
-    useEffect(() => {
-        let calculatedMonthData = calculateCurrentSums(data)
-        setMonthData(calculatedMonthData)
-    }, [data])
-
-    function calculateCurrentSums(data:any):MonthData{
-        let google_sum = 0;
-        let meta_sum = 0;
-        let influencer_sum = 0;
-        let revenue_sum = 0;
-
-        data.forEach((day:dataDay) => {
-            google_sum += day.google_spend
-            meta_sum += day.meta_spend
-            influencer_sum += day.influencer
-            revenue_sum += day.revenue
-        });
-
-        console.log(data)
-        return {
-            google_sum,
-            meta_sum,
-            influencer_sum,            
-            revenue_sum,
-        }
-    }
+    const [diffToPrevPeriod, setDiffToPrevPeriod] = useState<DiffToPrevPeriod>({
+        google_diff: 0,
+        meta_diff: 0,
+        influencer_diff: 0,            
+        revenue_diff: 0,
+    })
     interface MonthData {
         google_sum: number,
         meta_sum: number,
         influencer_sum: number,            
         revenue_sum: number,
     }
+    interface DiffToPrevPeriod {
+        google_diff: number,
+        meta_diff: number,
+        influencer_diff: number,            
+        revenue_diff: number,
+    }
+
+    const head1 = 'Google Spend'
+    const head2 = 'Facebook Spend'
+    const head3 = 'Influencer'
+    const head4 = 'Revenue'
+
+
+
+
+
+    useEffect(() => {
+        let calculatedMonthData = calculateCurrentSums(data)
+        function calculateCurrentSums(data:any):MonthData{
+            let google_sum = 0;
+            let meta_sum = 0;
+            let influencer_sum = 0;
+            let revenue_sum = 0;
+    
+            data.forEach((day:dataDay) => {
+                google_sum += day.google_spend
+                meta_sum += day.meta_spend
+                influencer_sum += day.influencer
+                revenue_sum += day.revenue
+            });
+    
+            console.log(data)
+            return {
+                google_sum,
+                meta_sum,
+                influencer_sum,            
+                revenue_sum,
+            }
+        }
+
+        function calculateDiffToPrevPeriod(data:any):DiffToPrevPeriod{
+            const periodLength = data.length
+            
+
+            return{
+                google_diff,
+                meta_diff,
+                influencer_diff,            
+                revenue_diff,
+            }
+        }
+        
+        setMonthData(calculatedMonthData)
+    }, [data])
+
+
 
     return(
         <div className="gridBox">
